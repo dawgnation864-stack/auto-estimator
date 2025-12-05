3038a67640428f31b8711f102aa237000ebab4ec
dawgnation864-stack <dawgnation864@gmail.com>
2025-12-05 02:50:17 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,9 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from database import Base
 
+class Ping(Base):
+    __tablename__ = "pings"
+    id = Column(Integer, primary_key=True)
+    message = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)

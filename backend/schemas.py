2e58ca4b7ef4791ce9c82de44c0d15b6d9c74a0a
dawgnation864-stack <dawgnation864@gmail.com>
2025-12-05 04:29:04 -0500

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel, EmailStr
 
+class RegisterRequest(BaseModel):
+    email: EmailStr
+    password: str
+    shop_name: str
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    role: str
+    shop_id: int
+
+    class Config:
+        orm_mode = True
